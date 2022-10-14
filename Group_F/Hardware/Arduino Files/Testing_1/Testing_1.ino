35e3ddd5a18eba8fb65721771b9e17abacdc5cf2
Dadoright <donabusi1@gmail.com>
2022-10-14 11:12:52 +0530

    the codes for the pin testing

---
+++ Group_F/Hardware/Arduino Files/Testing_1/Testing_1.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(D7,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(D7,HIGH);
+  delay(500);
+  
+  // put your main code here, to run repeatedly:
+
+}
