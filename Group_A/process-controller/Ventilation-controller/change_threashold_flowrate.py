c460349ee295a924b69d1d616994e23a96605ce7
thisaramanohara <thisaramanohara@gmail.com>
2022-10-14 11:43:30 +0530

    Broker changed

--- Group_A/process-controller/Ventilation-controller/change_threashold_flowrate.py
+++ Group_A/process-controller/Ventilation-controller/change_threashold_flowrate.py
@@ -10,7 +10,7 @@ client = mqtt.Client("Threashold flowrate value changer")
 
 topic = "326project/smartbuilding/hvac/change/flowrate-threash"
 
-client.connect("vpn.ce.pdn.ac.lk", port=8883)
+client.connect("10.40.18.10", port=1883)
 x = {
     "time": datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S'),
     "new-flowrate": random.uniform(0, 100)
